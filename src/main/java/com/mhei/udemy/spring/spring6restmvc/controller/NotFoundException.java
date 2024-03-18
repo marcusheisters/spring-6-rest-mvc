a27404307a5411ca2f284db5897287521227a1b0
Marcus Heisters <marcus.heisters@googlemail.com>
2024-03-18 20:39:05 +0100

    Exchange ExceptionController by @RespnseStatus annotation

--- src/main/java/com/mhei/udemy/spring/spring6restmvc/controller/NotFoundException.java
+++ src/main/java/com/mhei/udemy/spring/spring6restmvc/controller/NotFoundException.java
@@ -1,8 +1,12 @@
 package com.mhei.udemy.spring.spring6restmvc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author marcusheisters
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value Not Found")
 public class NotFoundException extends RuntimeException{
     public NotFoundException() {
     }
