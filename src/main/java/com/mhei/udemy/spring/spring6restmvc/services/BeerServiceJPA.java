34c45e482e51bb79b83582cc5381bde1ae907c58
marcusheisters <marcus.heisters@actaport.de>
2024-04-03 21:01:18 +0200

    Integration test for BeerController

---
+++ src/main/java/com/mhei/udemy/spring/spring6restmvc/services/BeerServiceJPA.java
@@ -0,0 +1,54 @@
+package com.mhei.udemy.spring.spring6restmvc.services;
+
+import com.mhei.udemy.spring.spring6restmvc.mappers.BeerMapper;
+import com.mhei.udemy.spring.spring6restmvc.model.BeerDTO;
+import com.mhei.udemy.spring.spring6restmvc.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService {
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers() {
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::beerToBeerDto)
+                .toList();
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+        return Optional.ofNullable(beerMapper.beerToBeerDto(beerRepository.findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beer) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+
+    }
+
+    @Override
+    public void updateBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+}
---
+++ src/test/java/com/mhei/udemy/spring/spring6restmvc/controller/BeerControllerIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.mhei.udemy.spring.spring6restmvc.controller;
+
+import com.mhei.udemy.spring.spring6restmvc.model.BeerDTO;
+import com.mhei.udemy.spring.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BeerControllerIntegrationTest {
+    @Autowired
+    BeerController beerController;
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> beers = beerController.listBeers();
+        assertThat(beers.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> beers = beerController.listBeers();
+        assertThat(beers.size()).isEqualTo(0);
+    }
+
+}
